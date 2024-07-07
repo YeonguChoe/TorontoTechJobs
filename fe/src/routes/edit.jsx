9c44462653189374bdd5af6b886a052ca55145a2
Yeongu Choe <yeonguchoe@Yeongus-MacBook-Air.local>
2024-07-06 20:43:44 -0400

    temporary fe

--- fe/src/routes/edit.jsx
+++ fe/src/routes/edit.jsx
@@ -1,23 +1,53 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import axios from "axios";
 
 export default function Edit() {
 
     let {postID} = useParams();
-    let [oldJSON, setOldJSON] = useState(null);
 
 
-    axios.get(`http://localhost:3000/jobs/${postID}`).then(res => {
-        console.log(res.data)
-    })
+    const [title, setTitle] = useState("");
+    const [company, setCompany] = useState("");
+    const [jobField, setJobField] = useState("");
+    const [lastEdit, setLastEdit] = useState("");
+    const [location, setLocation] = useState("");
+    const [description, setDescription] = useState("");
+
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/jobs/${postID}`).then(res => {
+            console.log(res.data);
+            setTitle(res.data.title);
+            setJobField(res.data.language);
+            setCompany(res.data.company);
+            setLastEdit(res.data.postedDate);
+            setLocation(res.data.location);
+            setDescription(res.data.description);
+        })
+    }, []);
+
+    function handleEditBtn() {
+
+        let newJSON = {
+            title: title,
+            company: company,
+            location: location,
+            language: jobField,
+            description: description
+        };
+
+        axios.patch(`http://localhost:3000/jobs/${postID}`, newJSON).then(res => {
+            console.log(res)
+        })
+    }
 
     return (
         <React.Fragment>
             <form>
                 <h1>Edit Post</h1>
                 <label htmlFor='job-field'>Job Field: </label>
-                <select id="job-field">
+                <select id="job-field" value={jobField} onChange={e => setJobField(e.target.value)}>
                     <option value='Backend'>Backend</option>
                     <option value='Frontend'>Frontend</option>
                     <option value='Mobile'>Mobile</option>
@@ -29,21 +59,21 @@ export default function Edit() {
                 <label>
                     Job Title:
                     <br/>
-                    <input type='text'
-                           required
-                    />
+                    <input type='text' defaultValue={title} onChange={setTitle} required/>
                 </label>
-                <br/>
+
+                <h5>Company: {company}</h5>
+                <h5>Last edited: {lastEdit}</h5>
                 <label>
                     Location: <br/>
-                    <input type='text' required/>
+                    <input type='text' defaultValue={location} onChange={setLocation} required/>
+                    <br/>
                 </label>
-                <br/>
 
-                <label>Job Explanation: <br/><textarea/></label>
+                <label>Job Explanation: <br/><textarea defaultValue={description} onChange={setDescription}/></label>
 
                 <br/>
-                <button type='submit'>Create Post</button>
+                <button type='submit' onClick={handleEditBtn}>Submit</button>
             </form>
         </React.Fragment>
     )
