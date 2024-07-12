f78052fc54d4b360a856099d1462d90bf5e65dfd
Minji Kim <minjikm19@gmail.com>
2024-07-12 15:35:08 -0400

    Fixed error

--- be/model/Company.js
+++ be/model/Company.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const companySchema = new mongoose.Schema({
-  compan_name: {
+  company_name: {
     type: String,
     required: true,
     unique: true,
